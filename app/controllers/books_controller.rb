c7a7f03e5a284aa395c352a0be221334004e348e
Eddie Kao <eddie@5xruby.tw>
2019-11-18 16:24:57 +0800

    add records to Book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,28 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params) 
+
+    if @book.save
+      redirect_to root_path, notice: '新增成功'
+    else
+      render html: params
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, 
+                                 :description, 
+                                 :list_price,
+                                 :sell_price,
+                                 :page_num,
+                                 :isbn,
+                                 :isbn13)
+  end
 end
