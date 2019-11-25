2fd503cc9d25d540f752f9b2d4335d9b9de8baad
Eddie Kao <eddie@5xruby.tw>
2019-11-25 15:01:45 +0800

    add date picker

---
+++ app/frontend/packs/datepicker.js
@@ -0,0 +1,8 @@
+import 'flatpickr/dist/flatpickr.min.css'
+
+import flatpickr from 'flatpickr'
+
+document.addEventListener('DOMContentLoaded', function(){
+  flatpickr('#book_published_at', {})
+})
+
