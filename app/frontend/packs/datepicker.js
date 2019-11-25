db4f5bc9e6ed0645a651a130d9a45de7f19dd9c6
Eddie Kao <eddie@5xruby.tw>
2019-11-25 17:00:30 +0800

    fix for turbolinks

--- app/frontend/packs/datepicker.js
+++ app/frontend/packs/datepicker.js
@@ -2,7 +2,7 @@ import 'flatpickr/dist/flatpickr.min.css'
 
 import flatpickr from 'flatpickr'
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('turbolinks:load', function(){
   flatpickr('#book_published_at', {})
 })
 
