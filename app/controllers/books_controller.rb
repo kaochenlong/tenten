0f6c70ed39bb76700512f61d66723826596a37b5
Eddie Kao <eddie@5xruby.tw>
2019-11-26 12:28:16 +0800

    add pagination with kaminari

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,10 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @books = Book.where(on_sell: true).with_attached_cover_image
-    @books = Book.available.with_attached_cover_image
+    @books = Book.available
+                 .with_attached_cover_image
+                 .page(params[:page])
+                 .per(4)
   end
 
   def show
