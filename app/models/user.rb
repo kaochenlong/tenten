4cb7ad652997e767d630f03c05bc422373b584e6
Eddie Kao <eddie@5xruby.tw>
2019-11-26 16:43:00 +0800

    update backend navbar link

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def employee?
+    role.in? ['staff', 'boss', 'admin']
+  end
 end
