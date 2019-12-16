4dfbc0c123f931fba00213a3e694af2b91dcd4d8
Eddie Kao <eddie@5xruby.tw>
2019-12-16 17:08:46 +0800

    Shopping Cart part 9

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
+
+  let(:cart) { Cart.new }
+
   it "每個 Cart Item 都可以計算它自己的金額（小計）" do
     # AAA
     # Arrange
-    cart = Cart.new
-
     book1 = create(:book, sell_price: 50)
     book2 = create(:book, sell_price: 100)
 
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,34 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+
+  let(:cart) { Cart.new }
+
   context "進階功能" do
     it "可以將購物車內容轉換成 Hash 並存到 Session 裡" do
-      cart = Cart.new
-
       book1 = create(:book)
       book2 = create(:book)
 
       3.times { cart.add_item(book1.id) }
       2.times { cart.add_item(book2.id) }
 
-      cart_hash = {
-        "items" => [
-          {"product_id" => 1, "quantity" => 3}, 
-          {"product_id" => 2, "quantity" => 2}
-        ]
-      }
-
       expect(cart.serialize).to eq cart_hash
     end
 
     it "也可以存放在 Session 的內容（Hash 格式），還原成購物車的內容" do
-      cart_hash = {
-        "items" => [
-          {"product_id" => 1, "quantity" => 3}, 
-          {"product_id" => 2, "quantity" => 2}
-        ]
-      }
-
       cart = Cart.from_hash(cart_hash)
       
       expect(cart.items.count).to be 2
@@ -38,14 +25,12 @@ RSpec.describe Cart, type: :model do
 
   context "基本功能" do
     it "可以把商品丟到到購物車裡，然後購物車裡就有東西了" do
-      cart = Cart.new  # PORO
       cart.add_item(1)
-      expect(cart.empty?).to be false
+      # expect(cart.empty?).to be false
+      expect(cart).not_to be_empty
     end
 
     it "加了相同的商品，項目不會增加，商品數量會改變" do
-      cart = Cart.new
-
       3.times { cart.add_item(1) }
       2.times { cart.add_item(2) }
 
@@ -54,7 +39,6 @@ RSpec.describe Cart, type: :model do
     end
     
     it "商品可以放到購物車裡，也可以再拿出來" do
-      cart = Cart.new
       book = create(:book)
 
       cart.add_item(book.id)
@@ -63,8 +47,6 @@ RSpec.describe Cart, type: :model do
 
     it "可以計算整台購物車的總消費金額" do
       # Arrange
-      cart = Cart.new
-
       book1 = create(:book, sell_price: 50)
       book2 = create(:book, sell_price: 100)
 
@@ -75,6 +57,16 @@ RSpec.describe Cart, type: :model do
       # Assert
       expect(cart.total_price).to eq 350
     end
+  end
+
+  private
+  def cart_hash
+    {
+      "items" => [
+        {"product_id" => 1, "quantity" => 3}, 
+        {"product_id" => 2, "quantity" => 2}
+      ]
+    }
   end
 end
 
