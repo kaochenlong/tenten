ddace733dfa0206605cc9da2d984c895026a828d
Eddie Kao <eddie@5xruby.tw>
2019-12-23 14:53:58 +0800

    move braintree script to header

--- app/frontend/packs/braintree.js
+++ app/frontend/packs/braintree.js
@@ -1,21 +1,25 @@
 import braintree from 'braintree-web-drop-in';
 
 document.addEventListener('turbolinks:load', () => {
-  let button = document.querySelector('#submit-button');
-  let token = document.querySelector('#token').value;
+  let container = document.querySelector('#dropin-container');
 
-  braintree.create({
-    authorization: token,
-    container: '#dropin-container'
-  }, function (createErr, instance) {
-    button.addEventListener('click', function () {
-      instance.requestPaymentMethod(function (err, payload) {
-        let hidden_field = document.querySelector('#nonce');
-        let form = document.querySelector('#payment-form');
-        hidden_field.value = payload.nonce;
-        form.submit();
+  if (container) {
+    let button = document.querySelector('#submit-button');
+    let token = document.querySelector('#token').value;
+
+    braintree.create({
+      authorization: token,
+      container: '#dropin-container'
+    }, function (createErr, instance) {
+      button.addEventListener('click', function () {
+        instance.requestPaymentMethod(function (err, payload) {
+          let hidden_field = document.querySelector('#nonce');
+          let form = document.querySelector('#payment-form');
+          hidden_field.value = payload.nonce;
+          form.submit();
+        });
       });
     });
-  });
+  }
 });
 
